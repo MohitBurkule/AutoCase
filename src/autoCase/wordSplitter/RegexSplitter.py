475de5ff19aae6e2e020f6da5c8740c7d79e7a7d
Mohit Burkule <Mohitburkule5@gmail.com>
2024-03-16 20:46:49 +0000

    Added simple word based RegexSplitter.py

--- src/autoCase/wordSplitter/RegexSplitter.py
+++ src/autoCase/wordSplitter/RegexSplitter.py
@@ -1,22 +1,32 @@
-#TODO: this uses user supplied or custon=m regex to select word groups or math spaces
-"""This is a Sample Python file."""
+def RegexSplitter(text, word_regex):
+    import re
+    return re.findall(word_regex, text)
 
-
-from __future__ import annotations
-
-
-def hello_world(i: int = 0) -> str:
-    """Doc String."""
-    print("hello world")
-    return f"string-{i}"
-
-
-def good_night() -> str:
-    """Doc String."""
-    print("good night")
-    return "string"
-
-
-def hello_goodbye():
-    hello_world(1)
-    good_night()
+if __name__ == "__main__":
+    word_regex = r'[A-Z][a-z]*'
+    text = "ThisIsASamplePythonFile"
+    print(RegexSplitter(text, word_regex) == ['This', 'Is', 'A', 'Sample', 'Python', 'File'])
+    text = "ThisIsASamplePython"
+    print(RegexSplitter(text, word_regex) == ['This', 'Is', 'A', 'Sample', 'Python'])
+    text = "ThisIsASamplePythonFile1"
+    print(RegexSplitter(text, word_regex) == ['This', 'Is', 'A', 'Sample', 'Python', 'File'])
+    text = "ThisIsASampleePython1File1"
+    print(RegexSplitter(text, word_regex) == ['This', 'Is', 'A', 'Samplee', 'Python', 'File'])
+    word_regex = r'[a-zA-Z]+'
+    text = "ThisIsASamplePythonFile"
+    print(RegexSplitter(text, word_regex) == ['ThisIsASamplePythonFile'])
+    text = "ThisIsASamplePython"
+    print(RegexSplitter(text, word_regex) == ['ThisIsASamplePython'])
+    text = "ThisIsASamplePythonFile1"
+    print(RegexSplitter(text, word_regex) == ['ThisIsASamplePythonFile'])
+    text = "ThisIsASampleePython1File1"
+    print(RegexSplitter(text, word_regex) == ['ThisIsASampleePython', 'File'])
+    word_regex = r'[a-z]+'
+    text = "ThisIsASamplePythonFile"
+    print(RegexSplitter(text, word_regex) == ['his', 's', 'ample', 'ython', 'ile'])
+    text = "ThisIsASamplePython"
+    print(RegexSplitter(text, word_regex) == ['his', 's', 'ample', 'ython'])
+    text = "ThisIsASamplePythonFile1"
+    print(RegexSplitter(text, word_regex) == ['his', 's', 'ample', 'ython', 'ile'])
+    text = "ThisIsASampleePython1File1"
+    print(RegexSplitter(text, word_regex) == ['his', 's', 'amplee', 'ython', 'ile'])
