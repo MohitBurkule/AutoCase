3f9b4cd0aa24fb3a8a00d93c872e6bd8ee3a4837
Mohit Burkule <Mohitburkule5@gmail.com>
2024-03-17 18:06:33 +0000

    black

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,13 +1,18 @@
 from __future__ import annotations
 
 from autoCase import camel, snake, kebab, title
-
+from autoCase.wordSplitter import SnakeSplitter
 camel_test = [
     ("game_engine_server_services_module_base", "gameEngineServerServicesModuleBase"),
+    ("game_engIne_server_services_module_base", "gameEngIneServerServicesModuleBase"),
+    ("game_engIne_ser-ver_services_module_base", "gameEngIneSerVerServicesModuleBase"),
     ("hiThere", "hiThere"),
+    ("hey There", "heyThere"),
+("hey there", "heyThere"),
+    ("hey;There", "heyThere"),
     ("hi123", "hi123"),
     ("hi_123", "hi123"),
-    ("hi_123_there", "hi123There"),
+    ("hi_123_tHere", "hi123THere"),
     ("hi__", "hi"),
     ("_hi", "hi"),
     ("_hi_", "hi"),
@@ -18,9 +23,27 @@ camel_test = [
     ("_hi_123_there_123", "hi123There123"),
     ("_hi_123_tHerRe_123_", "hi123THerRe123"),
 ]
+camel_from_snake = [
+    ("game_engine_server_services_module_base", "gameEngineServerServicesModuleBase"),
+    ("game_engIne_server_services_module_base", "gameEngIneServerServicesModuleBase"),
+("game_engIne_ser-ver_services_module_base", "gameEng-IneSer-verServicesModuleBase"),
+    ("hi_there", "HiThere"),
+    ("hey_there", "HeyThere"),
+    ("hi123", "Hi123"),
+    ("hi_123", "Hi123"),
+    ("hi_123_tHere", "Hi123THere"),
+    ("hi__", "Hi"),
+    ("_hi", "Hi"),
+    ("_hi_", "Hi"),
+    ("_hi_there", "HiThere"),
+    ("_hi__there", "HiThere"),
+    ("_hi_123_there", "Hi123There"),
+    ("_hi_123_there_", "Hi123There"),
+    ("_hi_123_there_123", "Hi123There123"),
+    ("_hi_123_tHerRe_123_", "Hi123THerRe123"),
+]
 
-
-def test_camel_test():
+def test_camel():
     """
     This defines the expected usage, which can then be used in various test cases.
     Pytest will not execute this code directly, since the function does not contain the suffex "test"
@@ -28,18 +51,8 @@ def test_camel_test():
     for test in camel_test:
         assert camel(test[0]) == test[1]
 
+def test_camel_from_snake():
+    for test in camel_from_snake:
+        assert camel(test[0], splitter=SnakeSplitter) == test[1],f"Failed for {test[0]}, expected {test[1]} but got {camel(test[0], splitter=SnakeSplitter)}"
 
-def test_hello(unit_test_mocks: None):
-    """
-    This is a simple test, which can use a mock to override online functionality.
-    unit_test_mocks: Fixture located in conftest.py, implictly imported via pytest.
-    """
-    pass  # hello_test()
-
-
-def test_int_hello():
-    """
-    This test is marked implicitly as an integration test because the name contains "_init_"
-    https://docs.pytest.org/en/6.2.x/example/markers.html#automatically-adding-markers-based-on-test-names
-    """
-    pass  # hello_test()
+test_camel_from_snake()
